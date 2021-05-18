e0d3de2b96ac1656ca053cd7243b90f8eb9a4f0d
ZaidAG <72582743+ZaidAG@users.noreply.github.com>
2021-05-18 11:44:50 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+require('@firbase/firestore')
+  var firebaseConfig = {
+    apiKey: "AIzaSyD_57GLu_fxQcWaNhRs6kpijAna5cucgUA",
+    authDomain: "book-santa-99291.firebaseapp.com",
+    projectId: "book-santa-99291",
+    storageBucket: "book-santa-99291.appspot.com",
+    messagingSenderId: "1041243617927",
+    appId: "1:1041243617927:web:e14191aff3f894e2251622",
+    measurementId: "G-SCCX832VVC"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  export default firebase.firestore()
